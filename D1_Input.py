5eec9923c5500e701c3e97489fe7416b90f20ebb
pablonxh <pablo.nxh@gmail.com>
2021-01-25 11:08:00 -0600

    D1 - Input + Variables

---
+++ D1_Input.py
@@ -0,0 +1,50 @@
+#Basic Input function
+
+'''
+input('Here:')
+n = input ('variable: ') '''
+
+
+#Write a program that prints the number of characters in a user's name.
+#You might need to Google for a function that calculates the length of a string.
+'''
+print(len(input("What's your name? ")))
+print("Hello " + input("What is your name?"))
+
+n = input("What's your first name?\n")
+print("Your name has", len(n), "characters.") '''
+
+#Write a program that switches the values stored in the variables a and b.
+#Warning. Do not change the code on lines 1-4 and 12-18. Your program should work for different inputs. e.g. any value of a and b.
+'''
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a2 = a
+a = b
+b = a2
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+'''
+
+#1. Create a greeting for your program.
+#2. Ask the user for the city that they grew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line, see the example at:
+'''
+print("Welcome to the Band Name Generator eartian!!!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+'''
