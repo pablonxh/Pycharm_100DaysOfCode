2fb7f053f2f0fd652ce229d1ef3996b6ff368676
pablonxh <pablo.nxh@gmail.com>
2021-01-25 10:18:01 -0600

    Test_Change

--- D1_Printing.py
+++ D1_Printing.py
@@ -1,5 +1,33 @@
+#Multiple print statements
+print('Hello World!')
+print("Hello World!") #Double Quotes
+
+#Single print statement line brakes
 print("Day 1 - Python Print Function\n"
 "The function is declared like this:\n"
 "print('what to print')")
 
-#Latest Commit
+#Single print statement with line break
+print('Hello World!\n\
+Hello World!')
+
+#Single print statement same line (no line break)
+print('Hello World!\
+ Hello World!')
+
+#To print without a new line in Python 3 add an extra argument to your print function
+# telling the program that you don’t want your next string to be on a new line.
+# Here’s an example: print("Hello there!", end = '') The next print function will be on the same line.
+
+print("Hello there!", end = '')
+print(" It is a great day.")
+
+print("Hello there!", end = ' Its Friday. ')
+print("It is a great day.")
+
+
+#Concatenate
+
+print("Hello" + " Pablo1")
+print("Hello " + "Pablo2")
+print("Hello" + " " + "Pablo3")
