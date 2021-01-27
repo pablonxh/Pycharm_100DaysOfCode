a2186e18b8b0054742efefd3b57ec6d13612ac05
pablonxh <pablo.nxh@gmail.com>
2021-01-26 21:18:25 -0600

    D4 - Done

--- D4 - Random.py
+++ D4 - Random.py
@@ -16,8 +16,8 @@ import random
 
 #write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
 
-coin = random.randint(0,1)
-if coin == 0:
-    print("Tails")
-else:
-    print("Heads")
+# coin = random.randint(0,1)
+# if coin == 0:
+#     print("Tails")
+# else:
+#     print("Heads")
