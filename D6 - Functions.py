e118d52761bcdd19291bb259a072518d0752c22e
pablonxh <pablo.nxh@gmail.com>
2021-02-01 18:39:50 -0600

    D6/7 - Done

---
+++ D6 - Functions.py
@@ -0,0 +1,59 @@
+#Functions
+
+# def newfunction():
+#     print("This is your functions")
+#     print(len("number"))
+#
+# newfunction()
+#
+#
+# #While Loops
+#
+# def circle():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     turn_left()
+#     move()
+#     circle()
+#     move()
+#     circle()
+#     move()
+#     turn_left()
+#
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     elif wall_in_front():
+#         jump()
+#
+#
+#
+# #Hurdle 4
+#
+#
+# def circle():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#
+#
+# while not at_goal():
+#     if right_is_clear():
+#         circle()
+#     else:
+#         if front_is_clear():
+#             move()
+#         else:
+#             turn_left()
+#
+#
+# def circle():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+
